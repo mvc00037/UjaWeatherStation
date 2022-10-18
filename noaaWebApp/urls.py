6b44b19239e63fa7a191743ac575445cc3f30ee0
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2022-10-18 23:51:08 +0200

    Cambio home

--- noaaWebApp/urls.py
+++ noaaWebApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from noaaWebApp.views import home,prediction,prueba,predictionNoaa19
 
 urlpatterns = [
-    path('', home, name='prediction'),
+    path('', home, name='home'),
     path('predictionNoaa19/', predictionNoaa19, name='prediction'),
     path('prueba/<str:idSat>', prueba, name='prueba'),
     path('admin/', admin.site.urls)
