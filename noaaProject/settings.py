b7a0438d9018e91dff41cc313ec8381a360a21ec
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-13 19:29:50 +0200

    Modify DEBUG

--- noaaProject/settings.py
+++ noaaProject/settings.py
@@ -131,8 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-if not DEBUG
+STATIC_URL = 'static/'
+
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
