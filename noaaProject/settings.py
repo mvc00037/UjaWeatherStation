072dfd28c81ad0a35572205e209a5200d105a355
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-13 21:57:01 +0200

    Create staticfiles

--- noaaProject/settings.py
+++ noaaProject/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
@@ -155,7 +154,6 @@ if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
