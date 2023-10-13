916c464131a16982e49fcebd7f7ce282848736e2
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-13 18:24:20 +0200

    Change wsgi(noaaProject.settings)

--- noaaWebApp/wsgi.py
+++ noaaWebApp/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import  get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","render_deploy.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","noaaProject.settings")
 
 application = get_wsgi_application()
